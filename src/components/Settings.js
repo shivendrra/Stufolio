10cab09c2deb325e102d4602986c760441ab4cf3
Shivendra <shivharsh44@gmail.com>
2023-11-06 00:35:20 +0530

    new modifiactions

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,25 +1,38 @@
 import React from 'react'
 
 export default function Settings() {
+
+  const LinkClick = () => {
+    const clickVal = document.getElementById('side-menu-btn').value;
+
+    console.log(clickVal);
+  }
+
   return (
     <>
       <div className="sett-sec">
         <div className="sett-bound">
           <div className="row">
-            <div className="col-lg-12">
-                <h2>Settings</h2>
-                <div className="col-lg-3">
-                  <h4>Menu</h4>
+            <div className="col-lg-12 col-sm-12 d-flex p-2">
+                <div className="col-lg-3 col-sm-3 bg-warning">
                   <div className="side-nav">
                     <ul>
-                      <li>General</li>
-                      <li>Profile</li>
-                      <li>Passwords</li>
-                      <li>Application</li>
+                      <li onClick={LinkClick} id='side-menu-btn' value={1} action='button'>
+                      Name
+                      </li>
+                      <li onClick={LinkClick} id='side-menu-btn' value={2} action='button'>
+                      Name
+                      </li>
+                      <li onClick={LinkClick} id='side-menu-btn' value={3} action='button'>
+                      Name
+                      </li>
+                      <li onClick={LinkClick} id='side-menu-btn' value={4} action='button'>
+                      Name
+                      </li>
                     </ul>
                   </div>
                 </div>
-                <div className="col-lg-9">
+                <div className="col-lg-9 col-sm-9 bg-primary">
                   <h4>Content</h4>
                 </div>
             </div>
